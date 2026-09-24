914ab050e7cf44183091b6212672136f24f000f0
Doug Burbidge <42259928+DougBurbidge@users.noreply.github.com>
2026-09-24 21:14:16 +0800

    Revert "On-screen reports: respect system dark mode."
    This reverts commit 578747ae8f3fa1d81ff4626e7bbab4e66dcd8c6d.

--- Controls/DisplayReport.cs
+++ Controls/DisplayReport.cs
@@ -45,26 +45,15 @@ namespace Torn5.Controls
 		{
 			RedrawTimer.Enabled = false;
 
-			if (!DesignMode)
-				Redraw();
+			if (Report == null)
+				BackgroundImage = null;
+			else if (!this.DesignMode)
+				BackgroundImage = Report.ToBitmap(Width, Height);
 		}
 
-		private bool IsDark()
-		{
-			try
-			{
-				return (int)Microsoft.Win32.Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "AppsUseLightTheme", 1) == 0;
-			}
-			catch
-			{
-				return false;
-			}
-		}
-        
 		public void Redraw()
 		{
-			BackColor = IsDark() ? Color.Black : Color.White;
-			BackgroundImage = Report?.ToBitmap(Width, Height, true);
+			BackgroundImage = Report.ToBitmap(Width, Height, true);
 		}
 	}
 }
